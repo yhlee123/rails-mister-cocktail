aad24b09fd98f83d13f17e448cf5d38ab7659d08
Young Lee <younghoonlee1205@gmail.com>
2018-06-08 17:41:24 +0200

    Added description to the cocktail model and reflected in view and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_08_142653) do
+ActiveRecord::Schema.define(version: 2018_06_08_152932) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2018_06_08_142653) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
   end
 
   create_table "doses", force: :cascade do |t|
